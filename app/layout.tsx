23ce6d955918de6b4d42172793fe0f51becde53a
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-12 12:44:52 -0300

    add Metadata to improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,20 @@ export const metadata: Metadata = {
   },
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://nextjs-dashboard-acme.vercel.app'),
+  authors: [{name: 'Mario Gonzalez', url:'https://www.linkedin.com/in/lllariogonzalez'}],
+  keywords: ['Next.js 14', 'Acme', 'Dashboard', 'nextjs.org/learn', 'Server Actions'],
+  openGraph: {
+    title: 'Acme Dashboard',
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    url: 'https://nextjs-dashboard-acme.vercel.app',
+    type: 'website',
+  },
+  twitter: {
+    site: '@acme',
+    description:'The official Next.js Learn Dashboard built with App Router.',
+    title:'Acme Dashboard by @lllariogonzalez',
+    creator:'@lllariogonzalez',
+  },
 };
 
 export default function RootLayout({
