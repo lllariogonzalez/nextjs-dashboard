208dae87b99d5203e017c85bbafe85b3fc74990a
Wentao <zhu.wentao@u.yale-nus.edu.sg>
2024-03-23 14:39:57 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/>
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to Carpool.</strong> This is the carpooling application for the{' '}
+            <a href="https://nusmods.com/courses/YSC3232/software-engineering" className="text-blue-500">
+              YSC3232 Software Engineering Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Group2 and Vercel.
           </p>
           <Link
             href="/login"
