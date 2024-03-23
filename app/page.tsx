4afa7c2310848153898c8287f34aad763ba57c22
Wentao <zhu.wentao@u.yale-nus.edu.sg>
2024-03-23 14:53:06 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -28,14 +28,14 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
+            src="/YSC3232_Walkthrough.png"
             width={1000}
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop and mobile versions"
           />
           <Image
-            src="/hero-mobile.png"
+            src="/YSC3232_Walkthrough.png"
             width={560}
             height={620}
             className="block md:hidden"
