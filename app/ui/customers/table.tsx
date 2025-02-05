53c833878d412d1316ed2058710785b2d02a60e3
lllariogonzalez <lllariogonzalez@gmail.com>
2025-02-04 21:40:27 -0300

    refactor(table.tsx): change import to use type for CustomersTable and FormattedCustomersTable

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
+import type { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
 import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export default async function CustomersTable({query}:{query: string}) {
